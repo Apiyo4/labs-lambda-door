132825a9c95d4f03077c06ef16a4452cf4ca294b
Annette Odingo <odingoannette@gmail.com>
2020-04-22 10:27:38 +0100

    Create Avatar component

--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Avatar } from './Avatar';
 
 export const UserProfile = ()=>{
     return(
         <div>
             <div>
-                Hello
+                <Avatar />
+
             </div>
         </div>
     )
