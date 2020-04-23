32d52fefce072723037be172bb5fcac9fb8cd0fa
Annette Odingo <odingoannette@gmail.com>
2020-04-23 12:58:15 +0100

    Add state to ensure that if edit is clicked then user details disapears

--- src/components/UserProfile/PersonalInfo.jsx
+++ src/components/UserProfile/PersonalInfo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 
 
@@ -16,29 +16,53 @@ margin-bottom:1%;
 const H4bold =  styled.h4`
     font-weight: bold;
 `
+const H4margin = styled.h4`
+    text-align: right;
+`
 export const PersonalInfo = () => {
+    const [isClose, setIsClose] = useState(true)
+ const openForm = ()=>{
+     setIsClose(!isClose);
+ }
+
     return (
-        <Div>
-            <Div1>
-                <h4>Full Name</h4>
-                <H4bold>josiah-williams</H4bold>
-            </Div1>
-            <Div1>
-                <h4>Username</h4>
-                <H4bold>josiah-williams</H4bold>
-                 </Div1>
-                 <Div1>
-                <h4>Email</h4>
-                <H4bold>josiahdamiwilliams@gmail.</H4bold> 
-                </Div1>
-                <Div1>
-                <h4>Age</h4>
-                <H4bold>20</H4bold>
-                </Div1>
-                <Div1>
-                <h4>Location</h4>
-                <H4bold>Nigeria</H4bold>
-                </Div1>
-        </Div>
+        <div>
+            <div>
+                <H4margin onClick={openForm}>Edit</H4margin>
+                {!isClose && (
+                    <div>
+                        Hi
+                    </div>
+                )
+                }       
+            </div>
+            {isClose &&(
+                <Div>
+                    <Div1>
+                        <h4>Full Name</h4>
+                        <H4bold>josiah-williams</H4bold>
+                    </Div1>
+                    <Div1>
+                        <h4>Username</h4>
+                        <H4bold>josiah-williams</H4bold>
+                    </Div1>
+                    <Div1>
+                        <h4>Email</h4>
+                        <H4bold>josiahdamiwilliams@gmail.</H4bold>
+                    </Div1>
+                    <Div1>
+                        <h4>Age</h4>
+                        <H4bold>20</H4bold>
+                    </Div1>
+                    <Div1>
+                        <h4>Location</h4>
+                        <H4bold>Nigeria</H4bold>
+                    </Div1>
+                </Div>
+            )
+            
+            }
+       
+        </div>
     )
 }
