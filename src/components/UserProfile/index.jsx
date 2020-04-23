01b806762230c5acd20b0cb9bc569bf162dd7872
Annette Odingo <odingoannette@gmail.com>
2020-04-23 09:40:07 +0100

    Made text bolder

--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Avatar } from './Avatar';
+import { PersonalInfo } from './PersonalInfo';
 
 export const UserProfile = ()=>{
     return(
         <div>
             <div>
                 <Avatar />
-
+                <PersonalInfo />
             </div>
         </div>
     )
