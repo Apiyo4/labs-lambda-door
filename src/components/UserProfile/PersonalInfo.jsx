9a811279ac31b17dcaaeb6998b9696eb138f6536
Annette Odingo <odingoannette@gmail.com>
2020-04-23 14:52:54 +0100

    Create UserForm Component and add code

--- src/components/UserProfile/PersonalInfo.jsx
+++ src/components/UserProfile/PersonalInfo.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components'
-
-
+import { UserForm } from './UserForm';
 
 const Div =  styled.div`
     display: flex;
     flex-wrap:wrap;
     justify-content: space-between;
-    margin-top:2%;
+    
 `
 const Div1 = styled.div`
 width: 50%;
@@ -19,19 +18,30 @@ const H4bold =  styled.h4`
 const H4margin = styled.h4`
     text-align: right;
 `
-export const PersonalInfo = () => {
-    const [isClose, setIsClose] = useState(true)
- const openForm = ()=>{
-     setIsClose(!isClose);
- }
-
-    return (
-        <div>
+const DivBorder =  styled.div`
+    border: 1px solid #ccc;
+    margin: auto;
+    margin-top:5%;
+    margin-bottom: 5%;
+    padding-left: 5%;
+    padding-right:5%;
+    padding-top:3%;
+    padding-bottom:3%;
+`
+export const PersonalInfo = ({openForm, isClose}) => {
+    
+     return (
+        <DivBorder>
             <div>
-                <H4margin onClick={openForm}>Edit</H4margin>
+                {isClose && 
+                    <H4margin> <span onClick={openForm}>Edit </span></H4margin>
+                }
                 {!isClose && (
                     <div>
-                        Hi
+                        <H4margin><span onClick={openForm}>Close</span></H4margin>
+                        <div>
+                            <UserForm />
+                    </div>
                     </div>
                 )
                 }       
@@ -48,7 +58,7 @@ export const PersonalInfo = () => {
                     </Div1>
                     <Div1>
                         <h4>Email</h4>
-                        <H4bold>josiahdamiwilliams@gmail.</H4bold>
+                        <H4bold>josiahdamiwilliams@gmail.com</H4bold>
                     </Div1>
                     <Div1>
                         <h4>Age</h4>
@@ -63,6 +73,6 @@ export const PersonalInfo = () => {
             
             }
        
-        </div>
+        </DivBorder>
     )
 }
--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar } from './Avatar';
 import { PersonalInfo } from './PersonalInfo';
 import { Links } from './Links';
-
+import styled from 'styled-components'
+const Div =  styled.div`
+ margin-left: 5%;
+ margin-right:5%;
+`
 export const UserProfile = ()=>{
+    const [isClose, setIsClose] = useState(true);
+    const openForm = () => {
+        setIsClose(!isClose);
+    }
     return(
-        <div>
+        <Div>
             <div>
                 <Avatar />
-                <PersonalInfo />
-                <Links />
+                <PersonalInfo isClose={isClose} openForm={openForm}/>
+                {
+                    isClose&&(
+                        <Links />
+                    )
+                }
+                
             </div>
-        </div>
+        </Div>
     )
 }
