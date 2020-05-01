34d9f22307eaade16b98e0ca48cef5bf7481626c
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-05-01 17:53:23 +0100

    fixed reducer error

--- src/pages/MyReviews/Dummy.jsx
+++ src/pages/MyReviews/Dummy.jsx
@@ -3,12 +3,12 @@ import { useEffect } from 'react';
 import Axios from 'axios';
 import { selectedUsers } from '../../state/actions/selectedusers';
 import { connect } from 'react-redux';
-const Dummy = ({selectedUsers,selectUserDetails}) =>{
-console.log(selectUserDetails)
-    const url = process.env.REACT_APP_BACKEND_URL + "/users/1";
-    useEffect(()=>{
-        selectedUsers(2)
-        
+const Dummy = ({ selectedUsers, selectUserDetails }) => {
+  console.log(selectUserDetails);
+  const url = process.env.REACT_APP_BACKEND_URL + '/users/1';
+  useEffect(() => {
+    selectedUsers(2);
+
     //     Axios.get(url).then(
     //         res =>{
     //             // debugger
@@ -18,20 +18,15 @@ console.log(selectUserDetails)
     //             // debugger
     //         }
     //     )
-    })
-    return(
-        <div>
-            Hello
-        </div>
-    )
-}
-const mapStateToProps =(state)=>{
-    return {
-        selectUserDetails:state.selectUserDetails 
-    }
-}
-export default connect(mapStateToProps,{selectedUsers})(Dummy);
-
+  }, []);
+  return <div>Hello</div>;
+};
+const mapStateToProps = state => {
+  return {
+    selectUserDetails: state.selectUserDetails,
+  };
+};
+export default connect(mapStateToProps, { selectedUsers })(Dummy);
 
 // state
 // { Full Name
@@ -43,5 +38,3 @@ export default connect(mapStateToProps,{selectedUsers})(Dummy);
 // Github Link
 // portfolio Link
 // }
-
-
---
+++ src/state/reducers/SelectedUser.js
@@ -0,0 +1,29 @@
+import * as types from '../types';
+
+const initialState = {
+  isLoading: false,
+  details: {},
+};
+export const selectedUserReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.GET_SELECTED_USER_DETAILS:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case types.GET_SELECTED_USER_DETAILS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        details: action.payload,
+      };
+    case types.GET_SELECTED_USER_DETAILS_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        details: action.payload,
+      };
+    default:
+      return state;
+  }
+};
