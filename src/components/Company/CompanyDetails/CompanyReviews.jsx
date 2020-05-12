3afe3d763a71440fd0c6817b773c9e729eae5adb
Annette Odingo <odingoannette@gmail.com>
2020-05-12 11:00:35 +0100

    Trying to fix codeclimate ba=ug

--- src/components/Company/CompanyDetails/CompanyReviews.jsx
+++ src/components/Company/CompanyDetails/CompanyReviews.jsx
@@ -62,7 +62,7 @@ export const CompanyReviews = ({ singleCompanyReviews}) => {
             <Empty
                 image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
                 imageStyle={{
-                    height: 60,
+                    height: 80,
                 }}
                 description={
                     <span className="text">
